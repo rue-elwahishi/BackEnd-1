7291fdbe4aae2fe9f1e359fb9ab6d6a59a9a9a69
muftah <muftah.elzawi@gmail.com>
2020-01-23 22:19:16 +0200

    securing DB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose')
+const connectDB = require('./config/db')
 const app = express();
 
 
@@ -8,20 +9,12 @@ const app = express();
 dotenv.config({
   path: './config/config.env'
 });
-// mongodb+srv://theOddOne:<password>@cluster0-64n5n.gcp.mongodb.net/test
-// DB Connection
-mongoose.connect('mongodb+srv://theOddOne:theOddOne113@cluster0-64n5n.gcp.mongodb.net/theOddOne', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('db is connected')
-});
+// DB Connection
+connectDB()
+
+
+
 
 const PORT = process.env.PORT || 8080;
 
