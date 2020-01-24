ddb486085a42a69a1642ae61390064ed1ab22915
insafmechergui <insaf_mechergui@outlook.com>
2020-01-24 10:03:31 +0100

    comment and like schema model

--- models/comment.js
+++ models/comment.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
   content: String,
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  status: true, //activated or desactivated
-  post_id: { type: mongoose.Schema.Types.ObjectId, ref: "Posts" },
-  comment_id: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" }
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  deactivated: Boolean,
+  post_id: { type: mongoose.Schema.Types.ObjectId, ref: "post" },
+  comment_id: { type: mongoose.Schema.Types.ObjectId, ref: "comment" }
 });
 
 mongoose.model("comment", commentSchema);
--- models/like.js
+++ models/like.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const likeSchema = new Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  event_id: { type: mongoose.Schema.Types.ObjectId, ref: "Events" },
-  post_id: { type: mongoose.Schema.Types.ObjectId, ref: "Posts" },
-  comment_id: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" }
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  event_id: { type: mongoose.Schema.Types.ObjectId, ref: "event" },
+  post_id: { type: mongoose.Schema.Types.ObjectId, ref: "post" },
+  comment_id: { type: mongoose.Schema.Types.ObjectId, ref: "comment" }
 });
 
 mongoose.model("like", likeSchema);
