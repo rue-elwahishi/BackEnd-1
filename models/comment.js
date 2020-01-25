4f4ed3472973cd81086f272e2112ea12c6ed0284
insafmechergui <insaf_mechergui@outlook.com>
2020-01-25 10:19:43 +0100

    comment CRUD functions

--- models/comment.js
+++ models/comment.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
   content: String,
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
   deactivated: Boolean,
-  post_id: { type: mongoose.Schema.Types.ObjectId, ref: "post" },
-  comment_id: { type: mongoose.Schema.Types.ObjectId, ref: "comment" }
+  post: { type: mongoose.Schema.Types.ObjectId, ref: "post" },
+  comment: { type: mongoose.Schema.Types.ObjectId, ref: "comment" }
 });
 
 mongoose.model("comment", commentSchema);
