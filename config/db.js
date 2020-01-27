63dfcc94095d97d08854cd4a40168982d9961f1e
hanialazreg <lazreghania@gmail.com>
2020-01-27 11:04:40 +0100

    post controller

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    var conn = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+    console.log(`Mongo is Connected`);
+  } catch (error) {
+    console.log(`Error ${e.message}`);
+  }
+};
+
+module.exports = connectDB;
