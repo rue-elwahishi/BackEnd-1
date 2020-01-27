5e484549bb326ea367c7e4d7d7e0d2cc58467637
insafmechergui <insaf_mechergui@outlook.com>
2020-01-27 10:49:48 +0100

    update of controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,52 +4,72 @@ const Comment = mongoose.model("comment");
 const bodyParser = require("body-parser");
 const express = require("express");
 
-// //create new comment
-// const createComment = callback => {
-//   var comment = new Comment({
-//     content: req.body.content,
-//     user: req.user._id,
-//     post: req.params.id
-//   });
-//   comment.save(callback);
-// };
+//create new comment
+const createComment = (req, res) => {
+  try {
+    var comment = new Comment({
+      content: req.body.content,
+      user: req.user._id,
+      post: req.params.id
+    });
+    comment.save();
+    res.json({ success: true, comment });
+  } catch (err) {
+    res.json({ success: false, err });
+  }
+};
 
-//get all comments
-var getAllComment = callback => {
-  Comment.find((err, data) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, data);
-    }
-  });
+//create reply comment
+var createReply = (req, res) => {
+  try {
+    Comment.create({
+      content: req.body.content,
+      user: req.user._id,
+      comment: req.params.id
+    });
+  } catch (err) {
+    res.json({ success: false, err });
+  }
+};
+
+//display comment
+var displayAll = async (req, res) => {
+  try {
+    var page = req.query.page;
+    var comments = await Comment.find({ post: req.params.id })
+      .limit(30)
+      .skip(page * 30);
+    res.json({ success: true, comments });
+  } catch (err) {
+    res.json({ success: false, err });
+  }
 };
 
 //update comment
-// const updateComment = (id, callback) => {
-//   Comment.findOneAndUpdate({ _id: id })
-//     .populate("comment")
-//     .exec((err, comment) => {
-//       if (err) {
-//         callback(err, null);
-//       } else {
-//         callback(null, comment);
-//       }
-//     });
-// };
-
-//delete comment
-const deleteComment = (id, callback) => {
-  Comment.deleteOne({ _id: id }, (err, result) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, result);
-    }
-  });
+const updateComment = (req, res) => {
+  try {
+    var comments = Comment.update(
+      { id: req.params.id },
+      { $set: { content: req.body.content } }
+    );
+    res.json({ success: true, comments });
+  } catch (err) {
+    res.json({ success: false, err });
+  }
 };
 
-// exports.createComment = createComment;
-exports.getAllComment = getAllComment;
-// exports.updateComment = updateComment;
+//delete comment
+const deleteComment = (req, res) => {
+  try {
+    Comment.deleteOne({ id: req.params.id });
+    res.json({ success: true });
+  } catch (err) {
+    res.json({ success: false, err });
+  }
+};
+
+exports.createComment = createComment;
+exports.createReply = createReply;
+exports.displayAll = displayAll;
+exports.updateComment = updateComment;
 exports.deleteComment = deleteComment;
--- routes/api/comment.js
+++ routes/api/comment.js
@@ -2,61 +2,20 @@ const mongoose = require("mongoose");
 const commentSchema = require("../models/comment");
 const Comment = mongoose.model("comment");
 const commentController = require("../../controllers/commentController");
-module.exports = app => {
-  //save  comment in database
-  app.post("/:id/comments", (req, res) => {
-    var comment = new Comment({
-      content: req.body.content,
-      user: req.user._id,
-      post: req.params.id
-    });
-    comment.save();
-  });
+const Router = (module.exports = require("express").Router());
 
-  //save reply for the comment
-  app.post("/comments/:id/reply", (req, res) => {
-    Comment.create({
-      content: req.body.content,
-      user: req.user._id,
-      comment: req.params.id
-    });
-  });
+//save  comment in database
+Router.post("/:id/comments");
 
-  //display all comment
+//save reply for the comment
+Router.post("/:id/reply");
 
-  app.get("/comments", (req, res) => {
-    Comment.getAllComment((err, result) => {
-      if (err) {
-        res.status(404).send(err);
-      } else {
-        res.status(200).send(result);
-        res.end();
-      }
-    });
-  });
+//display all comment
 
-  //update the comment
-  app.post("/comments/:id", (req, res) => {
-    Comment.update(
-      { id: req.params.id },
-      { $set: { content: req.body.content } },
-      (err, result) => {
-        if (err) {
-          res.send(err);
-        } else {
-          res.json("Comment is updated" + JSON.stringify(result));
-        }
-      }
-    );
-  });
+Router.get("/:id/comments");
 
-  //delete a comment
-  app.delete("/comments/:id", (req, res) => {
-    Comment.deleteOne({ id: req.params.id }, err => {
-      if (err) {
-        console.log(err);
-      }
-      res.send("comment is deleted");
-    });
-  });
-};
+//update the comment
+Router.post("/:id");
+
+//delete a comment
+Router.delete("/comments/:id");
