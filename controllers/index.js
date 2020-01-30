c40e580aba87ee867de26f5c51b52dac7c98a57d
rue-elwahishi <rubawahishi@gmail.com>
2020-01-30 10:44:36 +0100

    get posts in specific communites with likes and comments, also events with their location using Geonear

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,8 @@
 module.exports = {
-<<<<<<< HEAD
-=======
-
->>>>>>> 078fff755a51c6baeb48f0a7bfac64a8b23c831d
   UsersController: require("./users.js"),
   CommentsController: require("./comments.js"),
   PostsController: require("./posts.js"),
   CommunititesController: require("./communities.js"),
   FollowingsController: require("./following.js"),
-<<<<<<< HEAD
   EventsController: require("./events.js")
 };
-=======
-  EventsController: require("./events")
-};
-
->>>>>>> 078fff755a51c6baeb48f0a7bfac64a8b23c831d
