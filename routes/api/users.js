5fd1cff7ab1a3fc6a7cd961a1b30504f2f496937
rue-elwahishi <rubawahishi@gmail.com>
2020-01-31 10:17:03 +0100

    get user with their followers and followings done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,42 @@
 const express = require("express");
 const Router = express.Router();
-const {UsersController, PostsController} = require("../../controllers/index");
-const {CommunityMiddleware, AuthMiddleware} = require('../../helpers/index.js')
-
+const {
+  UsersController,
+  PostsController,
+  FollowingsController
+} = require("../../controllers/index");
+const {
+  CommunityMiddleware,
+  AuthMiddleware
+} = require("../../helpers/index.js");
 
 // Register
 Router.post("/register", UsersController.signUp);
 Router.post("/authenticate", UsersController.logIn);
 Router.get("/verify", UsersController.verifyToken);
-Router.get('/:id/posts', AuthMiddleware, CommunityMiddleware ,PostsController.getPostsByUserId)
-
-
+Router.get(
+  "/:id/posts",
+  AuthMiddleware,
+  CommunityMiddleware,
+  PostsController.getPostsByUserId
+);
+Router.get(
+  "/:username",
+  AuthMiddleware,
+  CommunityMiddleware,
+  UsersController.getUser
+);
+Router.get(
+  "/:id/followers",
+  AuthMiddleware,
+  CommunityMiddleware,
+  FollowingsController.getFollowers
+);
+Router.get(
+  "/:id/followings",
+  AuthMiddleware,
+  CommunityMiddleware,
+  FollowingsController.getFollowings
+);
 
 module.exports = Router;
